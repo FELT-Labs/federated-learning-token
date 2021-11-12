0bf9fc0ad994adff573b8ed86dc29b05c2b33d86
Breta01 <breta.hajek@gmail.com>
2021-11-12 17:21:04 +0800

    Init contract manager

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,16 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FELToken is ERC20 {
     // Referencing active contracts
-    address[] public contracts;
-
     constructor(uint256 initialSupply) ERC20("Federated Token", "FELT") public {
         _mint(msg.sender, initialSupply);
-    }
-
-    // TODO: Add contract creation function
-    function activateContract(address _contract) public returns(bool) {
-        // TODO: Check and transfer init resources
-        contracts.push(_contract);
-        return true;
     }
 }
