41b70a6ef5c65972d711e3005fe3d0c6819eebbe
Breta01 <breta.hajek@gmail.com>
2021-11-17 17:18:10 +0800

    Adding basic communication with project contract

--- felt/node/background_worker.py
+++ felt/node/background_worker.py
@@ -10,25 +10,29 @@ URL = "ws://localhost:8000/training/ws"
 ETH_NODE = "http://127.0.0.1:8545"
 
 
-async def producer(federated_contract):
+async def producer(project_contract):
     await asyncio.sleep(3)
+    print(project_contract)
     return "p"
 
 
 async def task():
     w3 = Web3(Web3.HTTPProvider(ETH_NODE))
-    print("Chain id", w3.eth.chain_id)
-    contracts = load_contracts(w3.eth.chain_id)
-    for k, c in contracts.items():
-        contracts[k]["obj"] = w3.eth.contract(address=c["address"], abi=c["abi"])
+    print("Worker connected to chain id: ", w3.eth.chain_id)
 
-    print("Fee:", contracts["ContractManager"]["obj"].functions.activationFee().call())
+    contracts = load_contracts(w3)
+    print(contracts)
+    project_contract = contracts["ProjectContract"]
+    print(project_contract.functions.isNewPlan().call())
+    print(project_contract.functions.isPublic().call())
+    print(project_contract.functions.latestPlan().call())
+
     # Infinite reconnect + run infinite connection - lovely :)
     while True:
         async with websockets.connect(URL) as websocket:
             try:
                 while True:
-                    cmd = await producer()
+                    cmd = await producer(project_contract)
                     # Request plan pull
                     await websocket.send(cmd)
                     rec = await websocket.recv()
