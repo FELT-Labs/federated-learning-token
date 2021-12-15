81b4a62c0e5667278f6d4dd75ed9e7aa64f3269e
Breta01 <breta.hajek@gmail.com>
2021-12-15 17:40:32 +0100

    Working on sidebar

--- webapp/src/components/sidebar.js
+++ webapp/src/components/sidebar.js
@@ -1,13 +1,39 @@
 import React, { useState } from "react";
-import { NavItem, NavLink, Nav, Button } from "reactstrap";
+import { Input, NavItem, NavLink, Nav, Button, Tooltip } from "reactstrap";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 import { useWeb3React } from "@web3-react/core";
-import { ChevronLeft, ChevronRight, User } from "react-feather";
+import { Box, ChevronLeft, ChevronRight, User } from "react-feather";
+
+import {
+	network,
+} from '../utils/connectors';
+
+
+
+const SupportedChains = [
+	{ id: 137, name: "Polygon Mainnet" },
+	{ id: 80001, name: "Matic Mumbai (testnet)" },
+	{ id: 1337, name: "Local Network (testnet)" },
+]
+
+
+
+class SimpleTooltip extends React.Component {
+  state = {isOpen: false};
+
+  toggle = () => {
+    this.setState({isOpen: !this.state.isOpen});
+  };
+
+  render () {
+    return <Tooltip isOpen={this.state.isOpen} toggle={this.toggle} {...this.props} />;
+  }
+}
 
 
 function Sidebar() {
-	const { account } = useWeb3React();
+	const { account, chainId, connector } = useWeb3React();
 	const [isOpen, toggle] = useState(true);
 
 	return (
@@ -16,15 +42,33 @@ function Sidebar() {
 				<Button className="toggleButton p-0" onClick={() => toggle(!isOpen)}
 					tag={isOpen ? ChevronLeft : ChevronRight}
 				/>
-				<div className="px-3">
-					<User color="white" />
-					<span className="p-3 align-middle">
+				<div className="row mb-3 px-3 align-items-center">
+					<User className="col-sm-2" color="white" />
+					<span className="col-sm-10">
 					{account
 						? `${account.substring(0, 6)}...${account.substring(account.length - 4)}`
 						: 'No account connected'}
 						</span>
 
 				</div>
+				<div id="chain-input" className="row px-3 align-items-center">
+					<Box className="col-sm-2" color="white" />
+					<div  class="col-sm-10">
+						<Input
+							name="select"
+							type="select"
+							value={chainId}
+							onChange={e => connector.changeChainId(e.target.value)}
+							disabled={connector !== network}
+						>
+							{SupportedChains.map(({id, name}) => (
+								<option key={id} value={id} selected={id === chainId}>{name}</option>
+							))}
+						</Input>
+					</div>
+				</div>
+				<SimpleTooltip placement="right" target="chain-input" >Valid grades are  blah, blah</SimpleTooltip>	
+				
 				<h3>Bootstrap Sidebar</h3>
 			</div>
 			<div className="side-menu">
