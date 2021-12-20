08ac65654e9b2450dbf48422a1f7abd3dbdfde86
Filip Masar <filip.masar99@gmail.com>
2021-12-20 12:06:54 +0100

    remove webapp tests

--- webapp/src/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import { render } from '@testing-library/react';
-// import App from '../views/Index';
-//
-// test('renders learn react link', () => {
-//   const { getByText } = render(<App />);
-//   const linkElement = getByText(/Getting started/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
