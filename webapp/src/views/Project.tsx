eaf74fc1b7bf5a4145dca78736223109e0c14df2
Breta01 <breta.hajek@gmail.com>
2021-12-21 18:59:43 +0100

    Working on displaying project contract

---
+++ webapp/src/views/Project.tsx
@@ -0,0 +1,71 @@
+import { FC, useState, useEffect } from 'react';
+import { Contract } from 'ethers';
+import { useParams } from 'react-router-dom';
+import { useWeb3React } from '@web3-react/core';
+import { Spinner } from 'reactstrap';
+
+import { getProjectContract } from '../utils/contracts.js';
+
+interface ContractProps {
+  contract: Contract;
+}
+
+const ContractDisplay: FC<ContractProps> = ({ contract }) => {
+  const [running, setRunning] = useState(-1);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // eslint-disable-next-line react/destructuring-assignment
+      try {
+        const r = await contract.getPlansLength();
+        setRunning(r);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    fetchData();
+  }, [contract]);
+
+  return (
+    <>
+      <p>Is running: {running.toString()}</p>
+      <p>Something else</p>
+    </>
+  );
+};
+
+const Project: FC = () => {
+  const { address } = useParams();
+  const [contract, setContract] = useState<Contract>();
+
+  const { library, chainId } = useWeb3React();
+
+  useEffect(() => {
+    let didCancel = false;
+
+    async function getContract() {
+      if (library) {
+        const c = await getProjectContract(address, library.getSigner());
+
+        if (c && !didCancel) {
+          setContract(c);
+        }
+      }
+    }
+
+    getContract();
+    return () => {
+      didCancel = true;
+    };
+  }, [library, chainId, address]);
+
+  return (
+    <div>
+      <h2>{address}</h2>
+      {contract ? <ContractDisplay {...{ contract }} /> : <Spinner />}
+    </div>
+  );
+};
+
+export default Project;
