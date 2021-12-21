35c74a7b419ddd8e6e2f081701aa0a4700888bd4
Breta01 <breta.hajek@gmail.com>
2021-12-21 18:59:43 +0100

    Working on displaying project contract

--- webapp/src/utils/contracts.js
+++ webapp/src/utils/contracts.js
@@ -1,46 +1,76 @@
-import { Contract, ContractFactory } from '@ethersproject/contracts';
+import { Contract, ContractFactory } from 'ethers';
 
-import map from "../artifacts/deployments/map.json"
-
+import map from '../artifacts/deployments/map.json';
 
 function getChain(chainId) {
-    return ((chainId === 1337) ? "dev" : chainId);
+  return chainId === 1337 ? 'dev' : chainId;
 }
 
 export function getContractAddress(chainId, name) {
-    let chain = getChain(chainId);
-    try {
-        return map[chain][name].at(-1);
-    } catch (e) {
-        console.log(`Couldn't find any deployed contract "${name}" on the chain "${chain}".`)
-        return undefined
-    }
+  const chain = getChain(chainId);
+  try {
+    return map[chain][name].at(-1);
+  } catch (e) {
+    console.log(
+      `Couldn't find any deployed contract "${name}" on the chain "${chain}".`,
+    );
+    return undefined;
+  }
 }
 
 export async function loadContract(chainId, name, library) {
-    let chain = getChain(chainId);
-    let address = getContractAddress(chainId, name);
+  const chain = getChain(chainId);
+  const address = getContractAddress(chainId, name);
 
-    let contractArtifact;
-    try {
-        contractArtifact = await import(`../artifacts/deployments/${chain}/${address}.json`)
-    } catch (e) {
-        console.log(`Failed to load contract artifact "../artifacts/deployments/${chain}/${address}.json"`)
-        return undefined
-    }
+  if (address === undefined) {
+    return undefined;
+  }
 
-    return new Contract(address.substr(2), contractArtifact.abi, library);
+  let contractArtifact;
+  try {
+    contractArtifact = await import(
+      `../artifacts/deployments/${chain}/${address}.json`
+    );
+  } catch (e) {
+    console.log(
+      `Failed to load contract artifact "../artifacts/deployments/${chain}/${address}.json"`,
+    );
+    return undefined;
+  }
+
+  return new Contract(address.substr(2), contractArtifact.abi, library);
 }
 
-
 export async function getContractFactory(name, signer) {
-    let contractArtifact;
-    try {
-        contractArtifact = await import(`../artifacts/contracts/${name}.json`);
-    } catch (e) {
-        console.log(`Failed to load contract artifact "../artifacts/contracts/${name}.json"`)
-        return undefined
-    }
+  let contractArtifact;
+  try {
+    contractArtifact = await import(`../artifacts/contracts/${name}.json`);
+  } catch (e) {
+    console.log(
+      `Failed to load contract artifact "../artifacts/contracts/${name}.json"`,
+    );
+    return undefined;
+  }
 
-    return new ContractFactory(contractArtifact.abi, contractArtifact.bytecode, signer);
+  return new ContractFactory(
+    contractArtifact.abi,
+    contractArtifact.bytecode,
+    signer,
+  );
+}
+
+export async function getProjectContract(address, library) {
+  let contractArtifact;
+  try {
+    contractArtifact = await import(
+      `../artifacts/contracts/ProjectContract.json`
+    );
+  } catch (e) {
+    console.log(
+      `Failed to load contract artifact "../artifacts/contracts/ProjectContract.json"`,
+    );
+    return undefined;
+  }
+
+  return new Contract(address, contractArtifact.abi, library);
 }
--- webapp/src/views/AppEth.js
+++ webapp/src/views/AppEth.js
@@ -1,124 +1,122 @@
 import React from 'react';
-import { Web3ReactProvider, useWeb3React, UnsupportedChainIdError } from '@web3-react/core';
 import {
-	NoEthereumProviderError,
-	UserRejectedRequestError as UserRejectedRequestErrorInjected
+  Web3ReactProvider,
+  useWeb3React,
+  UnsupportedChainIdError,
+} from '@web3-react/core';
+import {
+  NoEthereumProviderError,
+  UserRejectedRequestError as UserRejectedRequestErrorInjected,
 } from '@web3-react/injected-connector';
 import { UserRejectedRequestError as UserRejectedRequestErrorWalletConnect } from '@web3-react/walletconnect-connector';
 import { Web3Provider } from '@ethersproject/providers';
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
 import { useEagerConnect, useInactiveListener } from '../utils/hooks';
-import {
-	injected,
-	network,
-	walletconnect,
-} from '../utils/connectors';
+import { injected, network, walletconnect } from '../utils/connectors';
 
-import Projects from "./Projects";
-import Sidebar from "../components/sidebar";
-import HomeNavbar from "../components/navbar/HomeNavbar";
-import HomeFooter from "../components/footer/HomeFooter";
+import Projects from './Projects';
+import Sidebar from '../components/sidebar';
+import HomeNavbar from '../components/navbar/HomeNavbar';
+import HomeFooter from '../components/footer/HomeFooter';
 import { loadContract } from '../utils/contracts';
 import CreateProject from './CreateProject';
 import Project from './Project';
 
-
 const ConnectorNames = {
-	Injected: 'Injected',
-	Network: 'Network',
-	WalletConnect: 'WalletConnect',
+  Injected: 'Injected',
+  Network: 'Network',
+  WalletConnect: 'WalletConnect',
 };
 
-
 const connectorsByName = {
-	[ConnectorNames.Injected]: injected,
-	[ConnectorNames.Network]: network,
-	[ConnectorNames.WalletConnect]: walletconnect,
-}
-
+  [ConnectorNames.Injected]: injected,
+  [ConnectorNames.Network]: network,
+  [ConnectorNames.WalletConnect]: walletconnect,
+};
 
 function getErrorMessage(error) {
-	if (error instanceof NoEthereumProviderError) {
-		return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.'
-	} else if (error instanceof UnsupportedChainIdError) {
-		return "You're connected to an unsupported network."
-	} else if (
-		error instanceof UserRejectedRequestErrorInjected ||
-		error instanceof UserRejectedRequestErrorWalletConnect
-	) {
-		return 'Please authorize this website to access your Ethereum account.'
-	} else {
-		console.error(error)
-		return 'An unknown error occurred. Check the console for more details.'
-	}
+  if (error instanceof NoEthereumProviderError) {
+    return 'No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.';
+  } else if (error instanceof UnsupportedChainIdError) {
+    return "You're connected to an unsupported network.";
+  } else if (
+    error instanceof UserRejectedRequestErrorInjected ||
+    error instanceof UserRejectedRequestErrorWalletConnect
+  ) {
+    return 'Please authorize this website to access your Ethereum account.';
+  } else {
+    console.error(error);
+    return 'An unknown error occurred. Check the console for more details.';
+  }
 }
-
 
 function getLibrary(provider) {
-	const library = new Web3Provider(provider);
-	library.pollingInterval = 12000;
-	return library;
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 12000;
+  return library;
 }
 
-
 function AppEth() {
-	return (
-		<Web3ReactProvider getLibrary={getLibrary}>
-			<App />
-		</Web3ReactProvider>
-	)
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  );
 }
 export default AppEth;
 
-
 // TODO: Remove this component
 function App() {
-	const context = useWeb3React()
-	const { connector, library, account, activate, error } = context
+  const context = useWeb3React();
+  const { connector, library, account, activate, error } = context;
 
-	// handle logic to recognize the connector currently being activated
-	const [activatingConnector, setActivatingConnector] = React.useState()
-	React.useEffect(() => {
-		if (activatingConnector && activatingConnector === connector) {
-			setActivatingConnector(undefined)
-		}
+  // handle logic to recognize the connector currently being activated
+  const [activatingConnector, setActivatingConnector] = React.useState();
+  React.useEffect(() => {
+    if (activatingConnector && activatingConnector === connector) {
+      setActivatingConnector(undefined);
+    }
 
-		// Connect to network by default
-		if (!connector) {
-			setActivatingConnector(connectorsByName[ConnectorNames.Network])
-			activate(connectorsByName[ConnectorNames.Network])
-		}
-	}, [activatingConnector, connector, activate])
+    // Connect to network by default
+    if (!connector) {
+      setActivatingConnector(connectorsByName[ConnectorNames.Network]);
+      activate(connectorsByName[ConnectorNames.Network]);
+    }
+  }, [activatingConnector, connector, activate]);
 
-	// handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
-	const triedEager = useEagerConnect()
+  // handle logic to eagerly connect to the injected ethereum provider, if it exists and has granted access already
+  const triedEager = useEagerConnect();
 
-	// handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
-	useInactiveListener(!triedEager || !!activatingConnector)
+  // handle logic to connect in reaction to certain events on the injected ethereum provider, if it exists
+  useInactiveListener(!triedEager || !!activatingConnector);
 
-	const activateConnector = (name) => {
-		setActivatingConnector(connectorsByName[name]);
-		activate(connectorsByName[name]);
-	}
+  const activateConnector = (name) => {
+    setActivatingConnector(connectorsByName[name]);
+    activate(connectorsByName[name]);
+  };
 
-	const isActivating = (name) => activatingConnector !== connectorsByName[name];
+  const isActivating = (name) => activatingConnector !== connectorsByName[name];
 
-	return (
-		<>
-			<HomeNavbar />
-			<div className="d-flex">
-				<Sidebar {...{ isActivating, activateConnector }} />
-				<div className="w-100 sidebar-content">
-					<Routes>
-						<Route index element={<Projects />} />
-						<Route path="create-project" element={<CreateProject />} />
-						<Route path="project/:id" element={<Project />} />
-					</Routes>
-					{!!error && <h4 style={{ marginTop: '1rem', marginBottom: '0' }}>{getErrorMessage(error)}</h4>}
-				</div>
-			</div>
-			<HomeFooter />
-		</>
-	)
+  return (
+    <>
+      <HomeNavbar />
+      <div className="d-flex">
+        <Sidebar {...{ isActivating, activateConnector }} />
+        <div className="w-100 sidebar-content">
+          <Routes>
+            <Route index element={<Projects />} />
+            <Route path="create-project" element={<CreateProject />} />
+            <Route path="project/:address" element={<Project />} />
+          </Routes>
+          {!!error && (
+            <h4 style={{ marginTop: '1rem', marginBottom: '0' }}>
+              {getErrorMessage(error)}
+            </h4>
+          )}
+        </div>
+      </div>
+      <HomeFooter />
+    </>
+  );
 }
