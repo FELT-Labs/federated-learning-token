33a276a0da3d3cd4705908a3c38e2bc3402c3557
Martin Ondejka <ondejka99@gmail.com>
2022-02-06 19:39:04 +0100

    Add option to request join as builder

--- contracts/project/Builders.sol
+++ contracts/project/Builders.sol
@@ -12,14 +12,32 @@ contract Builders {
         bytes32 publicKey;
     }
 
-    mapping(address => Builder) public builders;
+    struct BuilderJoinRequest {
+        address _address;
+        bool parity;
+        bytes32 publicKey;
+    }
 
-    modifier onlyBuilder {
+    mapping(address => Builder) public builders;
+    address[] public buildersArray;
+
+    mapping(address => BuilderJoinRequest) public builderRequests;
+    address[] public builderRequestsArray;
+
+    modifier onlyBuilder() {
         require(
             builders[msg.sender]._address != address(0),
             "Only builders are allowed to execute this."
         );
         _;
+    }
+
+    function getBuildersLength() public view returns (uint256) {
+        return buildersArray.length;
+    }
+
+    function getBuilderRequestsLength() public view returns (uint256) {
+        return builderRequestsArray.length;
     }
 
     /**
@@ -28,9 +46,112 @@ contract Builders {
      * @param publicKey compressed public key value
      */
     function setBuilderPublickey(bool parity, bytes32 publicKey) public {
-        require(builders[msg.sender]._address == msg.sender, "Builder not set");
+        require(
+            builders[msg.sender]._address == msg.sender,
+            "Sender is not builder"
+        );
         builders[msg.sender].parity = parity;
         builders[msg.sender].publicKey = publicKey;
+    }
+
+    /**
+     * @notice Already accepted builder can add new builder.
+     * @param newBuilderAddress address of the new builder
+     * @param parity based on header value (0x02/0x03 - false/true)
+     * @param publicKey compressed public key value
+     */
+    function addBuilder(
+        address newBuilderAddress,
+        bool parity,
+        bytes32 publicKey
+    ) public onlyBuilder {
+        require(
+            builders[newBuilderAddress]._address == address(0),
+            "Builder already exists"
+        );
+
+        builders[newBuilderAddress] = Builder({
+            _address: newBuilderAddress,
+            parity: parity,
+            publicKey: publicKey
+        });
+
+        buildersArray.push(newBuilderAddress);
+    }
+
+    /**
+     * @notice Anyone can request to become a builder.
+     * @param parity based on header value (0x02/0x03 - false/true)
+     * @param publicKey compressed public key value
+     */
+    function requestJoinBuilder(bool parity, bytes32 publicKey) public {
+        require(
+            builders[msg.sender]._address == address(0),
+            "Builder already exists"
+        );
+        require(
+            builderRequests[msg.sender]._address == address(0),
+            "Builder already requested join"
+        );
+
+        builderRequests[msg.sender] = BuilderJoinRequest({
+            _address: msg.sender,
+            parity: parity,
+            publicKey: publicKey
+        });
+
+        builderRequestsArray.push(msg.sender);
+    }
+
+    /**
+     * @notice Already accepted builder can accept new builder.
+     * @param newBuilderAddress address of the new builder
+     */
+    function acceptBuilderJoinRequest(address newBuilderAddress)
+        public
+        onlyBuilder
+    {
+        require(
+            builderRequests[newBuilderAddress]._address != address(0),
+            "Address of new builder hasn't created request. Consider using addBuilder."
+        );
+
+        BuilderJoinRequest memory request = builderRequests[newBuilderAddress];
+
+        addBuilder(newBuilderAddress, request.parity, request.publicKey);
+
+        _removeRequest(newBuilderAddress);
+    }
+
+    /**
+     * @notice Already accepted builder can accept new builder.
+     * @param newBuilderAddress address of the new builder
+     */
+    function declineBuilderJoinRequest(address newBuilderAddress)
+        public
+        onlyBuilder
+    {
+        require(
+            builderRequests[newBuilderAddress]._address != address(0),
+            "Address of new builder hasn't created request. Consider using addBuilder."
+        );
+
+        _removeRequest(newBuilderAddress);
+    }
+
+    function _removeRequest(address builderAddress) private onlyBuilder {
+        for (uint256 i = 0; i < builderRequestsArray.length; i++) {
+            if (builderRequestsArray[i] == builderAddress) {
+                builderRequestsArray[i] = builderRequestsArray[
+                    builderRequestsArray.length - 1
+                ];
+                builderRequestsArray.pop();
+
+                delete builderRequests[builderAddress];
+
+                break;
+            }
+        }
     }
 
     // TODO - add builder function ?
--- tests/test_builders.py
+++ tests/test_builders.py
@@ -2,7 +2,7 @@ import brownie
 from coincurve import PrivateKey
 from scripts.helpful_scripts import get_account
 
-from felt.core.web3 import encrypt_secret, export_public_key
+from felt.core.web3 import export_public_key
 
 
 def test_owner_is_builder(project):
@@ -32,5 +32,118 @@ def test_set_builder_public_key(accounts, project):
     assert builder["publicKey"].hex() == public_key.hex()
     assert builder["_address"] == owner
 
-    with brownie.reverts("Builder not set"):
+    with brownie.reverts("Sender is not builder"):
         project.setBuilderPublickey(parity, public_key, {"from": non_builder})
+
+
+def test_add_builder(accounts, project):
+    # Arrange
+    owner = get_account()
+    new_builder_address = accounts[0]
+
+    # Act
+    test_key = PrivateKey()
+    parity, public_key = export_public_key(test_key.to_hex())
+    project.addBuilder(new_builder_address, parity, public_key, {"from": owner})
+
+    # Assert
+    assert project.getBuildersLength() == 2
+    assert project.buildersArray(0) == owner
+    assert project.buildersArray(1) == new_builder_address
+
+    new_builder = project.builders(new_builder_address).dict()
+    assert new_builder["_address"] == new_builder_address
+    assert new_builder["parity"] == parity
+    assert new_builder["publicKey"].hex() == public_key.hex()
+
+    with brownie.reverts("Builder already exists"):
+        project.addBuilder(new_builder_address, parity, public_key, {"from": owner})
+
+    with brownie.reverts("Only builders are allowed to execute this."):
+        project.addBuilder(
+            new_builder_address, parity, public_key, {"from": accounts[1]}
+        )
+
+
+def test_request_join_builder(accounts, project):
+    # Arrange
+    owner = get_account()
+    requestor = accounts[0]
+
+    # Act
+    test_key = PrivateKey()
+    parity, public_key = export_public_key(test_key.to_hex())
+    project.requestJoinBuilder(parity, public_key, {"from": requestor})
+
+    # Assert
+    assert project.getBuilderRequestsLength() == 1
+    assert project.builderRequestsArray(0) == requestor
+
+    request = project.builderRequests(requestor).dict()
+    assert request["_address"] == requestor
+    assert request["parity"] == parity
+    assert request["publicKey"].hex() == public_key.hex()
+
+    with brownie.reverts("Builder already requested join"):
+        project.requestJoinBuilder(parity, public_key, {"from": requestor})
+
+    with brownie.reverts("Builder already exists"):
+        project.requestJoinBuilder(parity, public_key, {"from": owner})
+
+
+def test_accept_request_join_builder(accounts, project):
+    # Arrange
+    owner = get_account()
+    requestor = accounts[0]
+
+    # Act
+    test_key = PrivateKey()
+    parity, public_key = export_public_key(test_key.to_hex())
+    project.requestJoinBuilder(parity, public_key, {"from": requestor})
+    project.acceptBuilderJoinRequest(requestor, {"from": owner})
+
+    # Assert
+    assert project.getBuilderRequestsLength() == 0
+
+    assert project.getBuildersLength() == 2
+    assert project.buildersArray(0) == owner
+    assert project.buildersArray(1) == requestor
+
+    new_builder = project.builders(requestor).dict()
+    assert new_builder["_address"] == requestor
+    assert new_builder["parity"] == parity
+    assert new_builder["publicKey"].hex() == public_key.hex()
+
+    with brownie.reverts(
+        "Address of new builder hasn't created request. Consider using addBuilder."
+    ):
+        project.acceptBuilderJoinRequest(accounts[1], {"from": owner})
+
+    with brownie.reverts("Only builders are allowed to execute this."):
+        project.acceptBuilderJoinRequest(requestor, {"from": accounts[1]})
+
+
+def test_decline_request_join_builder(accounts, project):
+    # Arrange
+    owner = get_account()
+    requestor = accounts[0]
+
+    # Act
+    test_key = PrivateKey()
+    parity, public_key = export_public_key(test_key.to_hex())
+    project.requestJoinBuilder(parity, public_key, {"from": requestor})
+    project.declineBuilderJoinRequest(requestor, {"from": owner})
+
+    # Assert
+    assert project.getBuilderRequestsLength() == 0
+
+    assert project.getBuildersLength() == 1
+    assert project.buildersArray(0) == owner
+
+    with brownie.reverts(
+        "Address of new builder hasn't created request. Consider using addBuilder."
+    ):
+        project.declineBuilderJoinRequest(accounts[1], {"from": owner})
+
+    with brownie.reverts("Only builders are allowed to execute this."):
+        project.declineBuilderJoinRequest(requestor, {"from": accounts[1]})
